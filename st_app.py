e48af4636640a9a5c7e3de0ff39e0d74065576a9
pragyanaischool <75975970+pragyanaischool@users.noreply.github.com>
2023-08-17 22:49:25 +0530

    Update st_app.py

--- st_app.py
+++ st_app.py
@@ -49,3 +49,12 @@ if genre == 'Comedy':
     st.write('You selected comedy.')
 else:
     st.write("You didn't select comedy.")
+
+# Usage of multiselect widget
+
+options = st.multiselect(
+    'What are your favorite colors',
+    ['Green', 'Yellow', 'Red', 'Blue'],
+    ['Yellow', 'Red'])
+
+st.write('You selected:', options)
